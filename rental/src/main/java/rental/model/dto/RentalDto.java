d6f59173116a13c68cac2ab1c171606ba2437925
minwoo817 <capzhang123@naver.com>
2025-02-11 18:07:38 +0900

    민우 병합

--- rental/src/main/java/rental/model/dto/RentalDto.java
+++ rental/src/main/java/rental/model/dto/RentalDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class RentalDto {
 	private int rno;
 	private String rdate;
@@ -19,12 +21,11 @@ public class RentalDto {
 	private String rreason;
 	private int mno;
 	private int sno;
-<<<<<<< HEAD
+
 	// + HTML에 출력할때 작성자의 회원번호가 아닌 작성자 ID, 가맹점 번호가 아닌 가맹점 이름 출력
 	private String mid;
-=======
-	
+
 	// 필요한 멤버변수는 추가해주세요.
->>>>>>> branch 'doha' of https://github.com/Kimdohaaa/rentalService
+
 	private String sname;
 }
